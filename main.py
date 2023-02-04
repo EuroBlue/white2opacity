c634e5e3c2d56d10a6563f4564780bd3e5d5be94
EuroBlue <reg@euroblue.dev>
2023-02-04 16:40:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from PIL import Image
+def main():
+    im = Image.open('in.png')
+    pixelMap = im.load()
+
+    img = Image.new(im.mode, im.size)
+    pixelsNew = img.load()
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            pixelsNew[i, j] = (pixelMap[i, j][0], pixelMap[i, j][1], pixelMap[i, j][1], 255-int((pixelMap[i, j][0]+pixelMap[i, j][1]+pixelMap[i, j][1])/3))
+    im.close()
+    img.show()
+    img.save("out.png")
+    img.close()
+
+
+if __name__ == '__main__':
+    main()
