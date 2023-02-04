821e62eddd88d516a1c54aa714e93f9cd634f020
EuroBlue <84800760+EuroBlue@users.noreply.github.com>
2023-02-04 23:09:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     pixelsNew = img.load()
     for i in range(img.size[0]):
         for j in range(img.size[1]):
-            pixelsNew[i, j] = (pixelMap[i, j][0], pixelMap[i, j][1], pixelMap[i, j][1], 255-int((pixelMap[i, j][0]+pixelMap[i, j][1]+pixelMap[i, j][1])/3))
+            pixelsNew[i, j] = (pixelMap[i, j][0], pixelMap[i, j][1], pixelMap[i, j][2], 255-int((pixelMap[i, j][0]+pixelMap[i, j][1]+pixelMap[i, j][1])/3))
     im.close()
     img.show()
     img.save("out.png")
